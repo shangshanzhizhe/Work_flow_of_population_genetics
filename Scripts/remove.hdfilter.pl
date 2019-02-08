6b4f7d7f56a22aad5ffdc27f8e0d4dbd61948920
shangshanzhizhe <1312108966@qq.com>
2019-02-08 21:48:01 +0800

    commit

--- Scripts/remove.hdfilter.pl
+++ Scripts/remove.hdfilter.pl
@@ -2,28 +2,28 @@
 
 =head1 Program Description
 
-This program is simply designed to filter the SNP and INDEL sites
-that are marked by the GATK Hard Filter algorithm.
-Delection of multi-altenative SNP sites can be chosen.
+    This program is simply designed to filter the SNP and INDEL sites
+    that are marked by the GATK Hard Filter algorithm.
+    Delection of multi-altenative SNP sites can be chosen.
 
 =head1 Contact & Version
 
-Author: Shangzhe Zhang, shangzhezhang@gmail.com
-Date: 2019.2.5
-Version: None
+    uthor: Shangzhe Zhang, shangzhezhang@gmail.com
+    Date: 2019.2.5
+    Version: None
 
 =head1 Command-line Option
 
---input <str> Input VCF format file, MUST be gziped;
---output <str> Output VCF format file, will be gziped by default;
---multi Set this option if you want to delete the multi-alt SNPs (DON'T use when --type INDEL)
---marker The marker you used in the Hard Filter Process, e.g. my_snp_filter
---type <str> Set the variant type: SNP or INDEL;
---help Show this help text
+    --input <str> Input VCF format file, MUST be gziped;
+    --output <str> Output VCF format file, will be gziped by default;
+    --multi Set this option if you want to delete the multi-alt SNPs (DON'T use when --type INDEL)
+    --marker The marker you used in the Hard Filter Process, e.g. my_snp_filter
+    --type <str> Set the variant type: SNP or INDEL;
+    --help Show this help text
 
 =head1 Usage Example
 
-remove.hdfilter.pl --input IN.vcf.gz --output OUT.vcf.gz --type SNP --marker my_snp_filter --multi
+    remove.hdfilter.pl --input IN.vcf.gz --output OUT.vcf.gz --type SNP --marker my_snp_filter --multi
 
 =cut
 
@@ -31,16 +31,16 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my （$infile, $outfile, $multi, $marker, $type, $help);
+my ($infile, $outfile, $multi, $marker, $type, $help);
 GetOptions(
     "input:s" =>\$infile,
     "output:s" =>\$outfile,
     "multi"=>\$multi,
     "help"=>\$help,
     "type:s"=>\$type
-)
-die `pod2text $0` if ($Help | @ARGV == 0 | ! $marker);
-undified($multi) if ($type eq "INDEL");
+)；
+die `pod2text $0` if ($Help || ! $marker);
+undef($multi) if ($type eq "INDEL");
 die `pod2text $0` if ($infile !~ /.*\.gz$/);
 if ($outfile !~ /.*\.gz$/){
     $outfile = $outfile.".gz";
@@ -54,6 +54,7 @@ while(<IN>){
         print OUT "$_\n";
         next;
     }
+    my @a = split/\s+/;
     next if ($a[6] eq $marker);
     if ($multi){
          next if ($a[4] =~ /,/);
